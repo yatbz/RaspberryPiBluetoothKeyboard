d40b100ec792eb15811c098d4e64ec8352234e63
yatbz <63602712+yatbz@users.noreply.github.com>
2020-11-14 15:08:50 +0100

    Update SendString_Start.py

--- SendString_Start.py
+++ SendString_Start.py
@@ -6,7 +6,7 @@ if (len(sys.argv) < 2):
 	print("No string to send provided")
 else:
 	if (IS.checkInstallationRun()):
-		print("Dependencies installed")
+		print("Dependencies installed or configfile updated.")
 		print("Please start script again")
 	else:
 		print("Please disconnect all bluetoothdevices first and connect if required from console")
